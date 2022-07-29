88d93a7cff6d6caab52d9e5b68225bf4a2b32066
Harsimar Singh Arora <harsimar_arora_ampba2022w@isb.edu>
2022-07-29 05:55:46 +0530

    setup file

---
+++ StreamlitFiles/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="StreamlitFiles",
+    version="0.0.1",
+    description="",
+    url="git@github.com:Harsimar-19/TextAnalytics-GroupProject.git",
+    author="Harsimar Singh Arora",
+    author_email="harsimar_arora_ampba2022w@isb.edu",
+    license="unlicense",
+    packages=["streamlit","pandas","regex","matplotlib","altair","nltk","sklearn","wordcloud","vaderSentiment","string"],
+    zip_safe=False
+    
+)
